4eae103572517a81c72e8e744870ddb9f021a9a8
Charllys Fernandes <charllys.daiany@gmail.com>
2023-01-01 19:04:28 -0300

    update index.html

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ let inputToDo = document.getElementById("inputTask");
 // console.log(inputToDo);
 
 inputToDo.addEventListener('change', function () {
-    console.log(inputToDo.innerText)
+    console.log(getTaskInput())
 })
+
+// function get innerHtml for inputTask
+
+function getTaskInput() {
+let inputToDo = document.getElementById("inputTask").value;
+
+    return inputToDo;
+}
+
+// function load index and save in Storage.
+
+function loadTask() {
+    
+}
