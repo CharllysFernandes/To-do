b3b8d1cba2a10fee8aa56ec6ccd25fc7effe50d9
Charllys Fernandes <charllys.daiany@gmail.com>
2023-01-02 01:05:44 -0300

    update index.html

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 let inputToDo = document.getElementById("addTask");
+let containerTask = document.getElementById('containerTask');
+let databaseStorage = JSON.parse(localStorage.getItem('database'))
 
-// console.log(inputToDo);
+if (databaseStorage !== null) {
+    for (let i = 0; i < databaseStorage.length; i++) {
+        containerTask.innerHTML +=
+            `
+        <div class="card-style-input">
+        <button onclick=completed(${databaseStorage[i].id})>
+        <i class="bi bi-square"></i>
+        </button>
+        <h1 id=label_${databaseStorage[i].id}>${databaseStorage[i].task}</h1>
+        <button>
+        <i class="bi bi-x-lg"></i>
+        </button>
+        </div>
+        `
+    }
+
+} else {
+    databaseStorage = [];
+}
 
 inputToDo.addEventListener('change', function () {
-    let inputToDo = document.getElementById("addTask").value;
-    let containerTask = document.getElementById('containerTask');
-    
+    let addTask = document.getElementById("addTask").value;
     containerTask.innerHTML +=
-    `
+        `
     <div class="card-style-input">
     <button>
         <i class="bi bi-square"></i>
     </button>
-    <h1>${inputToDo}</h1>
+    <h1>${addTask}</h1>
     <button>
         <i class="bi bi-x-lg"></i>
     </button>
     </div>
     `
     // console.log(getTaskInput())
+
+    databaseStorage.push({ id: `${databaseStorage.length + 1}`, task: `${addTask}` })
+    let storage = JSON.stringify(databaseStorage)
+    localStorage.setItem('database', storage)
+
 })
 
-// function get innerHtml for inputTask
+function completed(id) {
+    console.log( id)
+    let label = document.getElementById(`label_${id}`)
 
-function getTaskInput() {
-    let input = document.getElementById("addTask");
+    label.classList.add("text-decoration-line-through");
 
-
-}
-
-// function load index and save in Storage.
-
-function loadTask() {
-    
-}
-
-// Save
-
-function save(obj) {
-    localStorage('')
 }
