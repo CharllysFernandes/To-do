f5e53719c2adee0aea1f81b4993bc251f89ea76c
Charllys Fernandes <charllys.daiany@gmail.com>
2023-02-23 17:32:00 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+
 const card = (id, completed, task) => `
     <div class="card-style-input animate__animated animate__fadeIn">
         <button onclick="check(${id})">
-            <i class="bi ${completed === 'true' ? 'bi-check-square' : 'bi-square'}"></i>
+            <i class="bi ${completed ? 'bi-check-square' : 'bi-square'}"></i>
         </button>
-        <h1 id="label_${id}" class="${completed === 'false' ? '' : 'text-decoration-line-through text-muted'}">${task}</h1>
+        <h1 id="label_${id}" class="${completed ? 'text-decoration-line-through text-muted' : ''}">${task}</h1>
         <button onclick="remove(${id})">
             <i class="bi bi-x-lg"></i>
         </button>
@@ -17,20 +18,22 @@ const databaseStorage = JSON.parse(localStorage.getItem('todoDB')) || [];
 
 const renderTask = (databaseArray) => {
     if (databaseArray) {
-        databaseArray.forEach(({ id, completed, task }) => {
-            containerTask.innerHTML += card(id, completed, task);
-        });
+        const tasksHtml = databaseArray.map(({ id, completed, task }) => card(id, completed, task));
+        const allTasksHtml = tasksHtml.join('');
+        containerTask.innerHTML = allTasksHtml;
     }
 };
+
 
 const addTask = () => {
     const addTaskValue = inputToDo.value;
     const id = Math.floor(Math.random() * 100); // create random number 0 to 100
-    databaseStorage.push({ id: `${id * 1}`, task: `${addTaskValue}`, completed: `${false}` });
+    databaseStorage.push({ id: `${id * 1}`, task: `${addTaskValue}`, completed: false });
     save(databaseStorage);
 };
 
-const searchIndex = (value) => databaseStorage.findIndex(({ id }) => id === `${value}`);
+const searchIndex = id => databaseStorage.findIndex(({ id: taskId }) => taskId === `${id}`);
+
 
 const check = (id) => {
     const index = searchIndex(id);
@@ -38,10 +41,14 @@ const check = (id) => {
     save(databaseStorage);
 };
 
-const save = (databaseArray) => {
+
+const save = (databaseArray, shouldReloadPage = true) => {
     localStorage.setItem('todoDB', JSON.stringify(databaseArray));
-    window.location.reload();
+    if (shouldReloadPage) {
+        window.location.reload();
+    }
 };
+
 
 const remove = (id) => {
     const index = searchIndex(id);
